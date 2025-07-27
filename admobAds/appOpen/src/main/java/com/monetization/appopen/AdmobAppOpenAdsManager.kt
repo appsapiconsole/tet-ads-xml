c9e25ae2ee7ccd1afd8209dc7be652f80d1dfeed
Muhammad Ishfaq <ishfaqcompk6@gmail.com>
2025-07-27 21:38:29 +0500

    immersive mode added

--- admobAds/appOpen/src/main/java/com/monetization/appopen/AdmobAppOpenAdsManager.kt
+++ admobAds/appOpen/src/main/java/com/monetization/appopen/AdmobAppOpenAdsManager.kt
@@ -1,5 +1,6 @@
 package com.monetization.appopen
 
+import android.util.Log
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ProcessLifecycleOwner
@@ -10,6 +11,8 @@ import com.monetization.core.managers.AdmobBaseAdsManager
 object AdmobAppOpenAdsManager : AdmobBaseAdsManager<AdmobAppOpenAdsController>(AdType.AppOpen),
     DefaultLifecycleObserver {
 
+    private val TAG = "AdmobAppOpenAdsManagerTag"
+
     init {
         addNewController(AdmobAppOpenAdsController("Test", listOf(TestAds.TestAppOpenId)))
     }
@@ -19,37 +22,44 @@ object AdmobAppOpenAdsManager : AdmobBaseAdsManager<AdmobAppOpenAdsController>(A
 
     fun initAppOpen(callBack: AppOpenAndLifecycleListener) {
         listener = callBack
+        Log.d(TAG, "initAppOpen: ")
         ProcessLifecycleOwner.get().lifecycle.addObserver(this)
     }
 
     override fun onStart(owner: LifecycleOwner) {
         super.onStart(owner)
         listener?.onAppStart()
+        Log.d(TAG, "onStart: ")
     }
 
     override fun onStop(owner: LifecycleOwner) {
         super.onStop(owner)
         listener?.onAppStop()
+        Log.d(TAG, "onStop: ")
     }
 
     override fun onCreate(owner: LifecycleOwner) {
         super.onCreate(owner)
         listener?.onAppCreate()
+        Log.d(TAG, "onCreate: ")
     }
 
     override fun onDestroy(owner: LifecycleOwner) {
         super.onDestroy(owner)
         listener?.onAppDestroy()
+        Log.d(TAG, "onDestroy: ")
     }
 
     override fun onPause(owner: LifecycleOwner) {
         super.onPause(owner)
         listener?.onAppPause()
+        Log.d(TAG, "onPause: ")
     }
 
     override fun onResume(owner: LifecycleOwner) {
         super.onResume(owner)
         listener?.onAppResume()
+        Log.d(TAG, "onResume: ")
     }
 
 }
